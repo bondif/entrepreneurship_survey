2ddddccc10bddcd52e41662a84d2f03636ce43ae
BOUKHLIF Mohamed <boukhlifmohamed13@gmail.com>
2019-04-08 23:58:26 +0100

    feat: add google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,5 +76,15 @@
             @yield('content')
         </main>
     </div>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-137952556-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-137952556-1');
+    </script>
 </body>
 </html>
